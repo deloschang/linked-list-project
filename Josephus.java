5016c5d4083523ae6f6af558a7fb33937ed99b93
Delos Chang <lol.i.laugh@gmail.com>
2013-01-26 23:21:45 -0500

    adding identifiers for josephus problem, adding soldiers

--- Josephus.java
+++ Josephus.java
@@ -11,91 +11,122 @@ package PS2;
 import java.util.Scanner;
 
 
-public class Josephus<T> implements CS10LinkedList<T> {
-  private Element<T> sentinel; // sentinel, serves as head and tail
-  private Element<T> current;  // current position in the list
+public class Josephus {
+  private Soldier head;  // current position in the list
+  private Soldier current;  // current position in the list
  
   
   /**
    * A private inner class representing the elements in the list.
    */
-  private static class Element<T> {
+  private static class Soldier {
     // Because this is a private inner class, these can be seen from SentinelDLL,
     // but not from outside SentinelDLL.
-    private T data;                  // reference to data stored in this element
-    private Element<T> next;            // reference to next item in list
-    private Element<T> previous;        // reference to previous item in list
+    private int data;                  // reference to data stored in this element
+    private Soldier next;            // reference to next item in list
+    private Soldier previous;        // reference to previous item in list
     
     /**
      * Constructor for a linked list element, given an object.
      * @param obj the data in the element.
      */
-    public Element(T obj) {
+    public Soldier(int number) {
       next = previous = null;   // no element before or after this one, yet
-      data = obj;               // OK to copy reference, since obj references an immutable object
+      data = number;               // OK to copy reference, since obj references an immutable object
     }
 
     /**
      *  @return the String representation of a linked list element.
      */
     public String toString() {
-      return data.toString();
+      return String.valueOf(data);
     }
   }
 
   /**
    * Constructor for a circular, doubly linked list without sentinel
-   * Makes an empty list.
    * @param n number of soldiers
    * @param k skip successive executions
    */
   
   public Josephus(int n, int k) {
-    sentinel = new Element<T>(null);
-    clear();
+	// Initialize number of men 
+    start(n);
+    
+    // Remove every k men
+    remove(k);
   }
 
-  /*
-   * @see CS10LinkedList#clear()
+  /* Create number of men and links in doubly circular linked list
+   * @param n number of soldiers
    */
-  public void clear() {
-    // Make the list be empty by having the sentinel point to itself
-    // in both directions.
-    sentinel.next = sentinel.previous = sentinel;
-  
-    // There's only one place to put the current reference...
-    current = sentinel;
+  public void start(int n) {
+	Soldier temp_soldier = null;
+	
+	for (int i = 1; i < n + 1; i++){
+		Soldier soldier = new Soldier(n);
+		
+		// identify each soldier
+		soldier.data = i;
+		
+		System.out.println("Soldier is born: "+i+" data: "+soldier.data);
+		
+		// if first soldier, there is no link
+		if (i == 1){
+			// store reference
+			head = soldier;
+			temp_soldier = soldier;
+			continue;
+		}
+		
+		temp_soldier.next = soldier;
+		soldier.previous = temp_soldier;
+		
+		System.out.println("Previous Soldier's next: "+temp_soldier.next.data);
+		
+		// renew reference
+		temp_soldier = soldier;
+		
+		// End case 
+		if (i == n){
+			soldier.next = head;
+			head.previous = soldier;
+			continue;
+		}
+		System.out.println("Current Soldier's previous: "+soldier.previous.data);
+		
+	}
   }
 
-  /**
-   * @see CS10LinkedList#add()
+  /** Remove every k man from the list
+   * @param k skip successive executions
    */
-  public void add(T obj) {
-    Element<T> x = new Element<T>(obj);     // allocate a new element
-
-    // Splice in the new element.
-    x.next = current.next;
-    x.previous = current;
-    current.next.previous = x;
-    current.next = x;
-
-    current = x;                        // new element is current position
-  }
-
-  /**
-   *   * @see CS10LinkedList#remove()
-   */
-  public void remove() {
-    // Do not ever let the sentinel be deleted!
-    if (hasCurrent()) {          
-      // Splice out the current element.
-      current.previous.next = current.next;
-      current.next.previous = current.previous;
-        
-      current = current.next;           // make successor the new current
-    }
-    else 
-      System.err.println("No current item");
+  public void remove(int k) {
+//    Element<T> x = new Element<T>(obj);     // allocate a new element
+//
+//    // Splice in the new element.
+//    x.next = current.next;
+//    x.previous = current;
+//    current.next.previous = x;
+//    current.next = x;
+//
+//    current = x;                        // new element is current position
+//  }
+//
+//  /**
+//   *   * @see CS10LinkedList#remove()
+//   */
+//  public void remove() {
+//    // Do not ever let the sentinel be deleted!
+//    if (hasCurrent()) {          
+//      // Splice out the current element.
+//      current.previous.next = current.next;
+//      current.next.previous = current.previous;
+//        
+//      current = current.next;           // make successor the new current
+//    }
+//    else 
+//      System.err.println("No current item");
   }
 
   /**
@@ -104,178 +135,22 @@ public class Josephus<T> implements CS10LinkedList<T> {
   public String toString() {
     String result = "";
     
-    for (Element<T> x = sentinel.next; x != sentinel; x = x.next)
+    for (Soldier x = head; x != head.previous; x = x.next)
       result += x.toString() + "\n"; 
     
     return result;
   }
 
-  /**
-   * @see CS10LinkedList#contains()
-   */
-  public boolean contains(T obj) {
-    Element<T> x;
-  
-    // Since we have a sentinel that isn't being used for a "real" element,
-    // we put the object we are looking for in the sentinel.  That way, we
-    // know we'll find it some time during the search.
-    sentinel.data = obj;
-  
-    for (x = sentinel.next; !x.data.equals(obj); x = x.next)
-      ;
-  
-    // We dropped out of the loop because we found the target object in an element
-    // that x references.
-    // If we found it in the sentinel, it wasn't really in the list.
-    // If we found it before getting back to the sentinel, it was in the list.
-
-    // Put the sentinel back into its null state.
-    sentinel.data = null;
-
-    if (x == sentinel)
-      return false;             // tell the caller that we did not find the object
-    else {
-      current = x;              // remember where we found it
-      return true;              // and tell the caller
-    }
-  }
-
-  /**
-   * @see CS10LinkedList#isEmpty()
-   */
-  public boolean isEmpty() {
-    return sentinel.next == sentinel;
-  }
-  
-  /**
-   * @see CS10LinkedList#hasCurrent()
-   */
-  public boolean hasCurrent() {
-    return current != sentinel;
-  }
-  
-  /**
-   * @see CS10LinkedList#hasNext()
-   */
-  public boolean hasNext() {
-    return hasCurrent() && current.next != sentinel;
-  }
-
-  /**
-   * @see CS10LinkedList#getFirst()
-   */
-  public T getFirst() { 
-    if(isEmpty()) {
-      System.err.println("The list is empty");
-      return null;
-    }
-    current = sentinel.next;
-    return get();
-  }
-
-  /**
-   * @see CS10LinkedList#getLast()
-   */
-  public T getLast() {
-    if(isEmpty()) {
-      System.err.println("The list is empty");
-      return null;
-    }
-    current = sentinel.previous;
-    return get();
-  }
-
-  /**
-   * @see CS10LinkedList#addFirst()
-   */
-  public void addFirst(T obj) {
-    current = sentinel;
-    add(obj);
-  }
-
-  /**
-   * @see CS10LinkedList#addLast()
-   */
-  public void addLast(T obj) {
-    current = sentinel.previous;
-    add(obj);
-  }
-  
-  /**
-   * @see CS10LinkedList#get()
-   */
-  public T get() {
-    if (hasCurrent()) 
-      return current.data;
-    else {
-      System.err.println("No current item");
-      return null;
-    }
-
-  }
-  
-  /**
-   * @see CS10LinkedList#set()  
-   */
-  // Error if there is no current item.
-  public void set(T obj) {
-    if (hasCurrent())
-      current.data = obj;
-    else
-      System.err.println("No current item");
-  }
-  
-  /**
-   * @see CS10LinkedList#next()
-   */
-  // Moves current to the next position.  Error if there is no next item.
-  public T next() {
-    if (hasNext()) {
-      current = current.next;
-      return current.data;
-    }
-    else {
-      System.err.println("No next item");
-      return null;
-    }
-  }
-  
-  /**
-   * Appends the list "other" to the end of the list "this",
-   * and sets the "other" to an empty list. 
-   * the current element of "this" remains the current element of the combined list.
-   * @param other the list to be appended to this list.
-   */
-  public void append(SentinelDLL<T> other){
-	  if (!other.isEmpty()){
-		  Element<T> tail = sentinel.previous; // tail end of 1st list
-		  Element<T> otherHead = other.sentinel.next;
-		  Element<T> otherTail = other.sentinel.previous;
-		  
-		  // connect end of 1st list with head of 2nd list
-		  otherHead.previous = tail;
-		  tail.next = otherHead; 
-		  
-		  // connect end of 2nd list with beginning of 1st list
-		  sentinel.previous = otherTail; 
-		  otherTail.next = sentinel; 
-		  
-		  // make 2nd list empty 
-		  other.clear();
-	  } else { 
-		  // Handle edge case
-		  System.out.println("Other list is empty");
-	  }
-	  
-  }
-  
-  /**
-   * Test the append method for SA 4
-   * @param args
-   */
   public static void main(String [] args) {
-	  char command;           // a command
+	  int parameter_n, parameter_k ;           // a command
 	  Scanner input = new Scanner(System.in);
-      command = input.nextLine().charAt(0);
+	  
+      System.out.print("Enter number n of soldiers, at least 2: ");
+      parameter_n = input.nextInt();
+      
+      System.out.print("Enter spacing of victims, between 1 and n: ");
+      parameter_k = input.nextInt();
+      
+      new Josephus(parameter_n, parameter_k);
   }
 }
